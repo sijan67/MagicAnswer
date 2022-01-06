94fe0b8aa5e85e2d09c23fc1645cf53eb910c6d1
sijan67 <sijuichi1@gmail.com>
2022-01-06 15:14:51 +0900

    First Commit

--- Magic Answer/ViewController.swift
+++ Magic Answer/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ImageView1: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func Button1(_ sender: UIButton) {
+        
+        let imageList = [ UIImage(named: "ball1"), UIImage(named: "ball2"), UIImage(named: "ball3"), UIImage(named: "ball4"), UIImage(named: "ball5")]
+        ImageView1.image = imageList[Int.random(in: 0...4)]
+    
+}
 }
 
